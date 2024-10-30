3765cf399565017b6e372ae5db599fe0b96a8395
Matt Wilson <matt.wilson720@gmail.com>
2024-10-30 14:33:20 -0700

    Updated SDK to v1.7.2.rc-2

--- Plugins/InworldAI/Source/InworldAIIntegration/Public/InworldCharacterAudioComponent.h
+++ Plugins/InworldAI/Source/InworldAIIntegration/Public/InworldCharacterAudioComponent.h
@@ -24,6 +24,7 @@ class INWORLDAIINTEGRATION_API UInworldCharacterAudioComponent : public UAudioCo
 	
 public:
 	virtual void BeginPlay() override;
+	virtual void EndPlay(EEndPlayReason::Type Reason) override;
 
 	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnInworldCharacterVisemeBlendsUpdated, const FInworldCharacterVisemeBlends&, VisemeBlends);
 	UPROPERTY(BlueprintAssignable, Category = "EventDispatchers")
@@ -75,7 +76,7 @@ private:
 
 protected:
 	mutable FCriticalSection QueueLock;
-	TSharedPtr<FCharacterMessageUtteranceData> UtteranceData;
+	TSharedPtr<FCharacterMessageUtteranceDataAudio> UtteranceData;
 	int32 NumSoundDataBytesPlayed;
 	class USoundWaveProcedural* SoundStreaming;
 
